0c0088b1c7f5cb3bdd1261d5f2f68ffce8fdc6a3
lukihd <lukihd06@gmail.com>
2019-11-11 20:48:09 +0100

    update signin add signup

--- app/routes/signup.js
+++ app/routes/signup.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cactus Home' });
+  res.render('signup', {title: "Cactus Signup"});
 });
 
 module.exports = router;
